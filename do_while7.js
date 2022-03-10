a5d7222d00611fb9b415647a19c5baca2edc6d39
Laxmi Yadav <91363406+yadavlaxmi@users.noreply.github.com>
2022-03-10 16:57:01 +0530

    Add files via upload

---
+++ do_while7.js
@@ -0,0 +1,13 @@
+let readlineSync=require("readline-sync")
+var number=readlineSync.questionInt("enter a number==")
+var div=readlineSync.questionInt("enter a div==");
+var i=0
+do{
+  (i<number)
+  if (i%div==0){ 
+    console.log(i)
+  }
+  i++
+}
+  while (i<number)
+  
