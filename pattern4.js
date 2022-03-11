11a21d93b957582d50c5154b47790dc5db55cd0b
Laxmi Yadav <91363406+yadavlaxmi@users.noreply.github.com>
2022-03-11 21:04:26 +0530

    Add files via upload

---
+++ pattern4.js
@@ -0,0 +1,11 @@
+let n = 4; 
+let string = "";
+let count = 1;
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= i; j++) {
+    string += count;
+    count++;
+  }
+  string += "\n";
+}
+console.log(string);
